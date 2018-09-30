6d942e773b0d8399b17dedf0bc615eff9411635e
Cornus91 <j.cornus@gmail.com>
2018-09-30 14:15:52 +0200

    pierwszy commit

---
+++ zadanie1.py
@@ -0,0 +1,5 @@
+przysmak1 = input("Podaj nazwę ulubionego przysmaku: ")
+przysmak2 = input("Podaj nazwę drugiego ulubionego przysmaku: ")
+
+total = przysmak1 + przysmak2
+print("Ulubione przysmaki: \n", total)
