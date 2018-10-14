a2eca4a3b0c2717af9f5fea0296fb5e1364834da
Cornus91 <j.cornus@gmail.com>
2018-10-14 12:23:40 +0200

    zadanie_liczby

---
+++ zadanie_liczenie.py
@@ -0,0 +1,10 @@
+liczba1 = int(input("Podaj liczbę początkową "))
+liczba2 = int(input("Podaj liczbę końcową "))
+
+liczba3 = int(input("Podaj co ile wypisać "))
+
+for liczby in range (liczba1, liczba2, liczba3):
+	if liczby > liczba1:
+		print (liczby)
+	
+	
